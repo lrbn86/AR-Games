36e8d67a9a60ba839c68cbbdb2f5308a52e24520
lrbn86 <lrbn86@gmail.com>
2019-11-29 17:23:25 -0500

    Working on gallery for Store and Games pages

--- WWW/JS/index.js
+++ WWW/JS/index.js
@@ -1,15 +1,24 @@
 const barsIcon = document.getElementById("bars-icon");
 const navLinks = document.getElementById("nav-links");
 
+// TODO: This may or may not work properly. A workaround for now.
+if (window.innerWidth > 499) {
+  barsIcon.style.display = "none";
+} else {
+  barsIcon.style.display = "block";
+}
+
 // When the bars icon is clicked,
 // display the nav links and change the bars icon apperance
-barsIcon.addEventListener("click", function() {
-  // Check whether the nav links are already showing
-  if (barsIcon.className === "fa fa-2x fa-bars") {
-    barsIcon.className = "fa fa-2x fa-times";
-    navLinks.style.display = "block";
-  } else {
-    barsIcon.className = "fa fa-2x fa-bars";
-    navLinks.style.display = "none";
+barsIcon.addEventListener("click", function () {
+  if (window.innerWidth < 499) {
+    // Check whether the nav links are already showing
+    if (barsIcon.className === "fa fa-2x fa-bars") {
+      barsIcon.className = "fa fa-2x fa-times";
+      navLinks.style.display = "block";
+    } else {
+      barsIcon.className = "fa fa-2x fa-bars";
+      navLinks.style.display = "none";
+    }
   }
 });
